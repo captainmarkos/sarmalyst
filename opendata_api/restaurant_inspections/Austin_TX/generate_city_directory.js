7191e38fa412bc5a83aec3a361274d5e45d7bd14
Mark Brettin <mark@jackrabbitsystems.com>
2018-02-17 12:58:02 -0700

    Using the database to get data for templates.

--- opendata_api/restaurant_inspections/Austin_TX/generate_city_directory.js
+++ opendata_api/restaurant_inspections/Austin_TX/generate_city_directory.js
@@ -1,66 +1,41 @@
 var fs = require('fs');
+var mysql = require('mysql');
 var mustache = require('mustache');
+var utils = require('./utils');
+var dbutils = require('./dbutils');
 
-var single_file_name = function(r, suffix) {
-    suffix = suffix ? suffix : (new Date).getTime().toString();
-    var name = r.restaurant_name;
+var db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'bluewild'
+});
 
-    name = name.replace(/[\/\'\.\&\,\@\(\)\"\#]/g, '');
-    name = name.replace(/[\s+]/g, '-') +
-           '-' + suffix + '.html';
+var TEMPLATE_DIR = './templates/';
+var HTML_DIR = './html/';
+var OUTPUT_DIR = HTML_DIR + 'cities/';
 
-    name = name.replace(/^\-/, '');
+(function() {
+    var template = fs.readFileSync(TEMPLATE_DIR + 'cities_index.html', 'utf8');
 
-    return name;
-};
+    db.connect(dbutils.status);
 
-var gather_cities = function(data) {
-    var h = {};
-    for(var i = 0; i < data.length; i++) {
-        if(data[i].address_city) {
-            if(!h[data[i].address_city]) {
-                h[data[i].address_city] = [single_file_name(data[i], i)];
-            } else {
-                h[data[i].address_city].push(single_file_name(data[i], i));
-            }
+    var sql = 'SELECT * FROM restaurant_inspections GROUP BY facility_id ORDER BY name';
+    db.query(sql, function(err, data) {
+        var list = utils.gather_cities(data);
+
+        var content = mustache.render(template, { results: list });
+        fs.writeFileSync(HTML_DIR + 'cities_index.html', content);
+
+        for(var i = 0; i < list.length; i++) {
+            template = fs.readFileSync(TEMPLATE_DIR + 'city.html', 'utf8');
+            content = mustache.render(template, list[i]);
+
+            var file_name = list[i].city.toLowerCase().replace(/\s+/g, '-') + '.html';
+            fs.writeFileSync(OUTPUT_DIR + file_name, content);
         }
-    }
+    });
 
-    var a = [];
-    for(var prop in h) {
-        if(!h.hasOwnProperty(prop)) { continue; }
-
-        var item = {
-            'city': prop,
-            'city_file_name': prop.toLowerCase().replace(/\s+/g, '-') + '.html',
-            'files': h[prop]
-        };
-        a.push(item);
-    }
-
-    return a;
-};
-
-try {
-    var template = fs.readFileSync('templates/cities_index.html', 'utf8');
-
-    var json_data = fs.readFileSync('2017.json', 'utf8');
-
-    var data = JSON.parse(json_data);
-    var list = gather_cities(data);
-
-    var content = mustache.render(template, { results: list });
-    fs.writeFileSync('html/' + 'cities_index.html', content);
-
-    for(var i = 0; i < list.length; i++) {
-        template = fs.readFileSync('templates/city.html', 'utf8');
-        content = mustache.render(template, list[i]);
-
-        var file_name = list[i].city.toLowerCase().replace(/\s+/g, '-') + '.html';
-        fs.writeFileSync('html/cities/' + file_name, content);
-
-    }
-} catch(e) {
-    console.log('Exception caught: ' + e.toString());
-}
+    db.end(dbutils.status);
+})();
 
--- opendata_api/restaurant_inspections/Austin_TX/generate_singles.js
+++ opendata_api/restaurant_inspections/Austin_TX/generate_singles.js
@@ -1,60 +1,50 @@
 var fs = require('fs');
+var mysql = require('mysql');
 var mustache = require('mustache');
+var utils = require('./utils');
+var dbutils = require('./dbutils');
 
-var google_search_link = function(r) {
-    var query  = '';
-    if(r.restaurant_name) { r.restaurant_name.replace(/\s+/g, '+'); }
-    if(r.address_address) { query += '+' + r.address_address.replace(/\s+/g, '+'); }
-    if(r.address_city)    { query += '+' + r.address_city.replace(/\s+/g, '+'); }
-    if(r.address_state)   { query += '+' + r.address_state.replace(/\s+/g, '+'); }
+var db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'bluewild'
+});
 
-    return 'http://www.google.com/search?q=' + query;
-};
+var TEMPLATE_DIR = './templates/';
+var HTML_DIR = './html/';
+var OUTPUT_DIR = HTML_DIR + 'restaurants/';
 
-var single_file_name = function(r, suffix) {
-    suffix = suffix ? suffix : (new Date).getTime().toString();
-    var name = r.restaurant_name;
-
-    name = name.replace(/[\/\'\.\&\,\@\(\)\"\#]/g, '');
-    name = name.replace(/[\s+]/g, '-') +
-           '-' + suffix + '.html';
-
-    name = name.replace(/^\-/, '');
-
-    return name;
-};
-
-try {
-    var template = fs.readFileSync('templates/single.html', 'utf8');
-
-    var json_data = fs.readFileSync('2017.json', 'utf8');
-
+(function() {
+    var template = fs.readFileSync(TEMPLATE_DIR + 'single.html', 'utf8');
     var files_written = 0;
-    var data = JSON.parse(json_data);
-    console.log('--> data.length: ', data.length);
 
-    for(var i = 0; i < data.length; i++) {
-        data[i].inspection_date = data[i].inspection_date.match(/\d+-\d+-\d+/)[0];
-        data[i].restaurant_link_google = google_search_link(data[i]);
+    db.connect(dbutils.status);
 
-        var content = mustache.render(template, data[i]);
-        var restaurant_name = data[i].restaurant_name;
-        if(restaurant_name) {
-            var file_name = single_file_name(data[i], i);
+    var sql = 'SELECT * FROM restaurant_inspections GROUP BY facility_id ORDER BY name';
+    db.query(sql, function(err, data) {
+        for(var i = 0; i < data.length; i++) {
+            data[i].google_search_link = utils.google_search_link(data[i]);
 
-            fs.writeFileSync('html/restaurants/' + file_name, content);
-            files_written++;
+            var content = mustache.render(template, data[i]);
+            var name = utils.clean_restaurant_name(data[i].name);
+            //console.log('--> name: ', name);
+
+            if(name) {
+                var file_name = utils.single_file_name(name, i);
+                fs.writeFileSync(OUTPUT_DIR + file_name, content);
+                files_written++;
+            }
+            data[i].single_file_name = 'restaurants/' + file_name;
         }
-        data[i].single_file_name = 'restaurants/' + file_name;
-    }
 
-    template = fs.readFileSync('templates/singles_index.html', 'utf8');
-    content = mustache.render(template, { restaurants: data });
-    fs.writeFileSync('html/' + 'singles_index.html', content);
-    files_written++;
+        template = fs.readFileSync(TEMPLATE_DIR + 'singles_index.html', 'utf8');
+        content = mustache.render(template, { restaurants: data });
+        fs.writeFileSync(HTML_DIR + 'singles_index.html', content);
+        files_written++;
+        console.log('--> files_written', files_written);
+    });
 
-    console.log('--> files_written', files_written);
-} catch(e) {
-    console.log('Exception caught: ' + e.toString());
-}
+    db.end(dbutils.status);
+})();
 
